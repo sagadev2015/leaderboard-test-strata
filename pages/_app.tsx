a6e48a4ed5e182ab85ec39ff8418064d0b9fd710
SmartEngineer <94433565+sagadev2015@users.noreply.github.com>
2023-10-23 08:56:09 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import Header from '../components/layout/Header'
 import '../styles/globals.css'
 import { store } from '../store/index'
 
-const MyApp = ({ Component, pageProps }: AppProps) => {　
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Provider store={store}>
     <div className='bg-white min-h-screen'>
       <Header />
